fef31c3dc03fdfdd0e9413cc56a719c37bec29a0
tomas07221 <tomas.yamamoto@outlook.com>
2025-09-26 15:58:52 +0300

    more auth

--- ronin/ronin-schema.ts
+++ ronin/ronin-schema.ts
@@ -1,10 +1,49 @@
-import { model, string } from "ronin/schema"
+import { blob, boolean, date, link, model, string } from "blade/schema"
 
-export const UserModel = model({
+export const User = model({
   slug: "user",
   fields: {
-    name: string(),
-    email: string(),
+    email: string({ required: true, unique: true }),
+    emailVerified: boolean({ required: true }),
+    image: blob(),
+    name: string({ required: true }),
+  },
+})
+
+export const Session = model({
+  slug: "session",
+  fields: {
+    expiresAt: date({ required: true }),
+    ipAddress: string(),
+    token: string({ required: true, unique: true }),
+    userId: link({ required: true, target: "user" }),
+    userAgent: string(),
+  },
+})
+
+export const Account = model({
+  slug: "account",
+  pluralSlug: "accounts",
+  fields: {
+    accessToken: string(),
+    accessTokenExpiresAt: date(),
+    accountId: string({ required: true }),
+    idToken: string(),
     password: string(),
+    providerId: string({ required: true }),
+    refreshToken: string(),
+    refreshTokenExpiresAt: date(),
+    scope: string(),
+    userId: link({ required: true, target: "user" }),
+  },
+})
+
+export const Verification = model({
+  slug: "verification",
+  pluralSlug: "verifications",
+  fields: {
+    expiresAt: date({ required: true }),
+    identifier: string({ required: true }),
+    value: string({ required: true }),
   },
 })
